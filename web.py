fc2662c9649893bd258403fd2fcb488d4c96403b
william-song-shy <2560814258@qq.com>
2020-03-11 22:05:11 +0800

    Update

---
+++ web.py
@@ -0,0 +1,7 @@
+from flask import Flask,render_template,flash,request,redirect,url_for
+from flask_wtf import Form
+app=Flask (__name__)
+app.secret_key = os.environ.get('wen-wen-secretkey')
+@app.route ("/",methods=['GET','POST'])
+def main ():
+    return '咕咕咕'
