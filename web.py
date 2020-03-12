638496629101c32f12dfdd68b55fcba3b0eeaefb
william-song-shy <2560814258@qq.com>
2020-03-12 19:18:06 +0800

    Fix bugs

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,flash,request,redirect,url_for
 from flask_wtf import Form
+import os
 app=Flask (__name__)
 app.secret_key = os.environ.get('wen-wen-secretkey')
 @app.route ("/",methods=['GET','POST'])
