2baa07ae2ab270bfb73d47b0a955a39d2d571e81
william-song-shy <2560814258@qq.com>
2020-03-12 20:33:21 +0800

    Change to app.run()

--- web.py
+++ web.py
@@ -6,3 +6,5 @@ app.secret_key = os.environ.get('wen-wen-secretkey')
 @app.route ("/",methods=['GET','POST'])
 def main ():
     return '咕咕咕'
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
